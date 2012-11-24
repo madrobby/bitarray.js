97df540058e50d075e0f0baad0aefb3dc1436710
Forbes Lindesay <fpfl2@cam.ac.uk>
2012-11-24 13:41:07 +0000

    Support not using `new`

--- bitarray.js
+++ bitarray.js
@@ -1,6 +1,8 @@
+"use strict";
+
 function BitArray(size, value){
-  if (typeof this === undefined) return new BitArray(size, value);
-  if (value === undefined) value = 0;
+  if (typeof this === 'undefined') return new BitArray(size, value);
+  if (typeof value === 'undefined') value = 0;
   this.size = size;
   this.field = new Array(~~((size - 1) / BitArray.ELEMENT_WIDTH) + 1);
   for(var i = 0; i < this.field.length; i++) 
--- test.js
+++ test.js
@@ -2,7 +2,7 @@ var assert = require('better-assert');
 var BitArray = require('./');
 
 describe('a bit array of length 200', function () {
-  var ba = new BitArray(200);
+  var ba = BitArray(200);
   it('lets you set a value to 1', function () {
     ba.set(100, 1);
     assert(ba.get(100) === 1);
@@ -15,7 +15,7 @@ describe('a bit array of length 200', function () {
 
 describe('a bit array of length 20', function () {
   it('lets you set lots of values independently', function () {
-    var ba = new BitArray(20);
+    var ba = BitArray(20);
     [1,3,5,9,11,13,15].forEach(function(i){ ba.set(i, 1) });
     assert(ba.toString() === '01010100010101010000');
   });
@@ -23,7 +23,7 @@ describe('a bit array of length 20', function () {
 
 describe('a bit array with a default of 1', function () {
   it('lets you set a bit to 0', function () {
-    var ba = new BitArray(33,1);
+    var ba = BitArray(33,1);
     ba.set(0, 0)
     assert(ba.toString() == '011111111111111111111111111111111');
   });
